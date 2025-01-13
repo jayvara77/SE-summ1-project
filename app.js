58ca9faf459f437ec4cd0cb77869709a6fdb9591
Jay Vara <jayvara77@gmail.com>
2025-01-13 02:55:11 +0000

    Added feedback and score system for test question

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     const quizForm = document.getElementById('quizForm');
-    const scoreParagraph = document.getElementById('score') 
+    const scoreParagraph = document.getElementById('score');
 
     quizForm.addEventListener('submit', (event) => {
         event.preventDefault();
         
-        const formData = new FormData(quizForm)
-        const answerValue = formData.get('q1')
-        console.log(answerValue);
+        const correctAnswer = {
+            q1: 'Correct Answer'
+        };
 
-        let score = answerValue;
+        let score = 0;
+        const feedback = [];
+
+        const formData = new FormData(quizForm);
+        const answerValue = formData.get('q1');
         
-        scoreParagraph.innerHTML = `${score}`;
+        if (correctAnswer['q1'] === answerValue) {
+            score++;
+        } else {
+            feedback.push(`Question 1 was incorrect. Correct answer: ${correctAnswer['q1']}`);
+        }
+
+        if (feedback.length === 0) {
+            scoreParagraph.innerHTML = `Your score: ${score} out of 1, well done!`;
+        } else {
+            scoreParagraph.innerHTML = `Your score: ${score} out of 1. The correct answer was: ${correctAnswer['q1']}`;     
+        }
     });
-
-
-})
+});
